01a1aaf5f9b117aef9d5932b23d1b637b8ad18e4
Jorge Medina <jorgemedinaleigh@protonmail.com>
2022-04-22 18:29:44 -0400

    Created player & movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 20;
+    [SerializeField] float movementRange = 10;
+
+    float vertical;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ProcessMovement();
+    }
+
+    void ProcessMovement()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+
+        float yOffset = vertical * Time.deltaTime * movementSpeed;
+        float yPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(yPos, -movementRange, movementRange);
+
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+    }
+}
