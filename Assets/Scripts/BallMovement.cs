a7bcc7937107acd16eebe440419c93c6562ff13c
Jorge Medina <jorgemedinaleigh@protonmail.com>
2022-04-26 18:55:15 -0400

    Make the ball speed up on each collision

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     [SerializeField] float ballSpeed = 20f;
+    [SerializeField] float speedIncrement = 1.01f;
+    float speed;
     Vector3 startDirection;
+    Vector3 lastPosition;
     Rigidbody ballRb;
 
     void Start()
     {
         ballRb = GetComponent<Rigidbody>();
         StartBallMovement();
+        lastPosition = transform.position;
     }
 
     void Update()
@@ -19,9 +23,23 @@ public class BallMovement : MonoBehaviour
         ballRb.velocity = ballRb.velocity.normalized * ballSpeed;
     }
 
+    private void FixedUpdate()
+    {
+        speed = Mathf.Lerp(speed, (transform.position - lastPosition).magnitude / Time.deltaTime, 0.7f);
+        lastPosition = transform.position;
+        Debug.Log("Speed: " + speed);
+    }
+
     void StartBallMovement()
     {
         startDirection = Random.insideUnitCircle.normalized;
         ballRb.velocity = startDirection * ballSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+        ballSpeed = ballSpeed * speedIncrement;
+        ballRb.velocity = ballRb.velocity.normalized * ballSpeed;
     }
 }
